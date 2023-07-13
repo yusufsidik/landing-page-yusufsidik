b06bf5d0f5749d945809c012b76903e1889acc87
yusufsidik <yusufsidik36@gmail.com>
2023-07-13 14:19:43 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center bg-gradient-to-b from-sky-300 to-sky-500">
+      <h5 className="text-white text-2xl text-center font-bold">
+        Coming Soon - Landing Page
+      </h5>
+    </div>
+  );
+}
+
+export default App;
